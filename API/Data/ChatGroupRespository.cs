c83658fc334c0539f68bf1e095e2ba83b6d87898
mattcoder2020 <mattcoder2013@yahoo.com>
2023-11-02 07:27:29 +0800

    one more

--- API/Data/ChatGroupRespository.cs
+++ API/Data/ChatGroupRespository.cs
@@ -14,12 +14,17 @@ namespace API.Data
         }
         public async Task AddMemberToChatGroupAsync(int userid, int chatgroupid)
         {
-            _dbContext.ch
+            await _dbContext.ChatGroupMembers.AddAsync(new ChatGroupMember { AppUserId = userid, ChatGroupId = chatgroupid });
         }
 
-        public async Task AddMessageAsync(ChatGroupMessage message)
+        public async Task AddMessageAsync(int senderid, int chatgroupid, string content)
         {
-            throw new NotImplementedException();
+            await _dbContext.ChatGroupMessages.AddAsync(new ChatGroupMessage
+            {
+                SenderId = senderid,
+                ChatGroupId = chatgroupid,
+                Content = content
+            });
         }
 
         public async Task<ChatGroup> GetChatGroupByNameAsync(string chatgroupName)
@@ -28,6 +33,13 @@ namespace API.Data
         }
 
         public async Task<IEnumerable<AppUser>> GetMemberByChatGroupAsync(string chatgroupName)
+        {
+            var querable = _dbContext.ChatGroups.Include(e => e.ChatGroupMembers).FirstOrDefaultAsync(e => e.Name == chatgroupName);
+            return querable.Result.ChatGroupMembers;
+            
+        }
+
+        public Task<IEnumerable<AppUser>> GetMemberByChatGroupAsync(int chatgroupid)
         {
             throw new NotImplementedException();
         }
